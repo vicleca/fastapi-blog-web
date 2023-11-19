0053209a0b88bdbdb7c58ee89550ea58fcf5dbdd
vicleca <victorleocanto@gmail.com>
2023-11-19 17:40:47 +0100

    Add create post logic

--- src/NewPost.js
+++ src/NewPost.js
@@ -10,10 +10,78 @@ function NewPost() {
   const[title, setTitle] = useState('')
   const[text, setText] = useState('')
 
+  const handleImageUpload = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0])
+    }
+  }
+
+  const handleCreate = (e) => {
+    e?.preventDefault()
+
+    const formData = new FormData()
+    formData.append('image', image)
+
+    const requestOptions = {
+      method: 'POST',
+      body: formData
+    }
+
+    fetch(BASE_URL + 'post/image', requestOptions)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        createPost(data.filename)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setImage(null)
+        document.getElementById('fileInput').value = null
+      })
+  }
+
+  const createPost = (imageUrl) => {
+    const json_string = JSON.stringify({
+      'image_url': imageUrl,
+      'title': title,
+      'content': text,
+      'creator': creator
+    })
+
+    const requestOptions = {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: json_string
+    }
+
+    fetch(BASE_URL + 'post', requestOptions)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        window.location.reload()
+        window.scrollTo(0, 0)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   return (
     <div className="newpost_content">
       <div className="newpost_image">
-        <input type="file" id="fileInput" onChange={null} />
+        <input type="file" id="fileInput" onChange={handleImageUpload} />
       </div>
       <div className="newpost_creator">
         <input className="newpost_creator" type="text" id="creator_input"
@@ -31,7 +99,7 @@ function NewPost() {
           value={text} />
       </div>
       <div>
-        <button className="create_button" onClick={null}>Create</button>
+        <button className="create_button" onClick={handleCreate}>Create</button>
       </div>
     </div>
   )
