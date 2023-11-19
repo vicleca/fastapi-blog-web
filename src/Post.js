eda59d10df0f50b062c812ca4a327ee53c4fd9b2
vicleca <victorleocanto@gmail.com>
2023-11-19 17:01:05 +0100

    Add posts list

---
+++ src/Post.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import './Post.css'
+
+const BASE_URL = 'http://localhost:8000/'
+
+function Post({post}) {
+
+  const [imageUrl, setImageUrl] = useState('')
+
+  useEffect(() => {
+    setImageUrl(BASE_URL + post.image_url)
+  }, [])
+
+  return (
+    <div className='post'>
+      <img className='post_image' src={imageUrl}/>
+      <div className='post_content'>
+        <div className='post_title'>{post.title}</div>
+        <div className='post_creator'>by {post.creator}</div>
+        <div className='post_text'>{post.content}</div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Post
